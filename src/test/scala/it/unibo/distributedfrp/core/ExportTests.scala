c500a92d7b5422e89dc616ee4f78ad387a0e809a
francescodente <dente.francesco1996@gmail.com>
2023-01-25 11:11:51 +0100

    Add followPath() tests

--- src/test/scala/it/unibo/distributedfrp/core/ExportTests.scala
+++ src/test/scala/it/unibo/distributedfrp/core/ExportTests.scala
@@ -7,15 +7,57 @@ import matchers._
 class ExportTests extends AnyFlatSpec with should.Matchers:
 
   "A generic export" should "have the given root" in {
-    Export(10, Map.empty).root should be (10)
+    Export(1, Map.empty).root should be (1)
   }
 
   it should "have the given children" in {
-    val children = Map[Any, Export[Any]](
-      "a" -> Export.atomic(1),
-      "b" -> Export.atomic(2),
+    val children: Map[Any, Export[Any]] = Map(
+      "a" -> Export.atomic(10),
+      "b" -> Export.atomic(20),
     )
-    Export(10, children).children should be (children)
+    Export(1, children).children should be (children)
+  }
+
+  it should "be traversable in zero steps" in {
+    val exp = Export(1, Map.empty)
+    exp.followPath(Seq.empty) should be (Some(exp))
+  }
+
+  it should "be traversable to its immediate children" in {
+    val child = Export.atomic(10)
+    val exp = Export(1, Map(
+      "a" -> child,
+      "b" -> Export.atomic(2)
+    ))
+    exp.followPath(Seq("a")) should be (Some(child))
+  }
+
+  it should "be traversable to nested children" in {
+    val child = Export.atomic(15)
+    val exp = Export(1, Map(
+      "a" -> Export(2, Map(
+        "aa" -> Export(3, Map(
+          "aaa" -> child,
+          "aab" -> Export.atomic(4),
+        )),
+        "ab" -> Export.atomic(5),
+      )),
+      "b" -> Export.atomic(6)
+    ))
+    exp.followPath(Seq("a", "aa", "aaa")) should be(Some(child))
+  }
+
+  it should "not be traversable with an invalid path" in {
+    val exp = Export(1, Map(
+      "a" -> Export(2, Map(
+        "aa" -> Export.atomic(3),
+        "ab" -> Export.atomic(4),
+      )),
+      "b" -> Export.atomic(5)
+    ))
+    exp.followPath(Seq("c")) should be (None)
+    exp.followPath(Seq("a", "ac")) should be (None)
+    exp.followPath(Seq("a", "aa", "aaa")) should be (None)
   }
 
   "An atomic export" should "have the given root" in {
