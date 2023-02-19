1eefa36c0f9c11b41389f510281f4ad70e56ae8c
francescodente <dente.francesco1996@gmail.com>
2023-02-19 17:42:20 +0100

    Remove unused Program trait

--- src/main/scala/it/unibo/distributedfrp/incarnation/Incarnation.scala
+++ src/main/scala/it/unibo/distributedfrp/incarnation/Incarnation.scala
@@ -4,6 +4,3 @@ import it.unibo.distributedfrp.core.*
 
 trait Incarnation extends Core, Language, Semantics, CoreExtensions:
   def context(selfId: DeviceId): Context
-  
-  trait Program[A]:
-    def main: Flow[A]
