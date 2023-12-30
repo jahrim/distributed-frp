41a6c0f7cbbfb449c172426a1a52dc55e8f20f23
jahrim <jahrim.cesario2@studio.unibo.it>
2023-12-30 20:59:16 +0100

    fix: change IncrementalCellSink to be thread-safe

--- src/main/scala/it/unibo/distributedfrp/frp/IncrementalCellSink.scala
+++ src/main/scala/it/unibo/distributedfrp/frp/IncrementalCellSink.scala
@@ -3,15 +3,16 @@ package it.unibo.distributedfrp.frp
 import nz.sodium.{Cell, CellSink}
 
 class IncrementalCellSink[A](initValue: A, calm: Boolean = false):
-  var currentValue: A = initValue
+  private var currentValue: A = initValue
   private val cellSink: CellSink[A] = new CellSink(initValue)
 
-  def cell: Cell[A] = cellSink
+  def cell: Cell[A] = this.cellSink
 
-  def set(a: A): Unit = update(_ => a)
+  def set(a: A): Unit = this.update(_ => a)
 
   def update(f: A => A): Unit =
-    val old = currentValue
-    currentValue = f(currentValue)
-    if !calm || !old.equals(currentValue) then cellSink.send(currentValue)
-
+    synchronized {
+      val old = this.currentValue
+      this.currentValue = f(this.currentValue)
+      if !calm || old != this.currentValue then this.cellSink.send(this.currentValue)
+    }
