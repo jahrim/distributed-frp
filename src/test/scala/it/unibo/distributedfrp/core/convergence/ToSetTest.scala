92127534fa9cb97cdfe6aedadc561e92abd94887
jahrim <jahrim.cesario2@studio.unibo.it>
2023-12-30 20:54:13 +0100

    test: add convergence tests on the 'toSet' construct

---
+++ src/test/scala/it/unibo/distributedfrp/core/convergence/ToSetTest.scala
@@ -0,0 +1,18 @@
+package it.unibo.distributedfrp.core.convergence
+
+/** A [[ConvergenceTest]] for the toSet construct. */
+class ToSetTest extends ConvergenceTest.WithDefaults:
+  private val ToSet = symbol("toSet")
+  
+  import defaultSimulator.incarnation.{*, given}
+
+  private def collectNeighbors: Flow[Set[DeviceId]] = nbr(mid).toSet
+  ToSet should "collect the computations of the neighboring devices in a set" in convergenceTest(
+    simulator = defaultSimulator,
+    flow = collectNeighbors,
+    limit = Map(
+      0 -> Set(0, 1, 3, 4),       1 -> Set(0, 1, 2, 3, 4, 5),          2 -> Set(1, 2, 4, 5),
+      3 -> Set(0, 1, 3, 4, 6, 7), 4 -> Set(0, 1, 2, 3, 4, 5, 6, 7, 8), 5 -> Set(1, 2, 4, 5, 7, 8),
+      6 -> Set(3, 4, 6, 7),       7 -> Set(3, 4, 5, 6, 7, 8),          8 -> Set(4, 5, 7, 8)
+    ),
+  )
