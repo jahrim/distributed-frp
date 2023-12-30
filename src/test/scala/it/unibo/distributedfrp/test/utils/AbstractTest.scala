f3cb79e5a69eef83ec912e643d465569b7bf37cf
jahrim <jahrim.cesario2@studio.unibo.it>
2023-12-30 20:51:23 +0100

    chore: add round functionality to AbstractTest for setting the precision of doubles during testing

--- src/test/scala/it/unibo/distributedfrp/test/utils/AbstractTest.scala
+++ src/test/scala/it/unibo/distributedfrp/test/utils/AbstractTest.scala
@@ -10,4 +10,17 @@ trait AbstractTest extends AnyFlatSpec with Matchers with BeforeAndAfter with In
    * @param name the specified name.
    * @return a symbol with the specified name.
    */
-  def symbol(name: String): String = s"`$name`"
+  protected def symbol(name: String): String = s"`$name`"
+
+  extension (self: Double){
+    /**
+     * @param precision the specified number of decimals.
+     * @return the [[Double]] with the specified number of decimals that
+     *         is closest to this [[Double]]. If this [[Double]] is not
+     *         finite, return this [[Double]] unchanged.
+     */
+    def round(precision: Int): Double = self match
+      case d if d.isFinite => BigDecimal(self).setScale(precision, BigDecimal.RoundingMode.HALF_UP).toDouble
+      case _ => self
+  }
+
