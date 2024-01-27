4c027362fbc45ec01bc2d704995a8c2e173e83bf
jahrim <jahrim.cesario2@studio.unibo.it>
2024-01-27 22:19:38 +0100

    test: fix ExecutionContextSpyTest replacing non-deterministic tests

--- src/test/scala/it/unibo/distributedfrp/test/utils/executor/ExecutionContextSpy.scala
+++ src/test/scala/it/unibo/distributedfrp/test/utils/executor/ExecutionContextSpy.scala
@@ -8,7 +8,7 @@ import scala.concurrent.ExecutionContext
  */
 trait ExecutionContextSpy extends ExecutionContext:
   /** @return the number of [[Thread Thread]]s that are currently executing a task. */
-  def busyThreads: Long
+  def busyThreads: Int
 
   /**
    * @return the maximum number of [[Thread Thread]]s that have been executing tasks
@@ -17,6 +17,15 @@ trait ExecutionContextSpy extends ExecutionContext:
    *       possibility of blocking tasks.
    */
   def maxConcurrency: Int
+
+  /**
+   * @return the number of tasks that are in queue for execution in this
+   *         [[ExecutionContext]].
+   */
+  def pendingTasks: Int
+
+  /** Wait for all pending tasks to be completed. */
+  def awaitTaskCompletion(): Unit
 
 /** Companion object of [[ExecutionContext]]. */
 object ExecutionContextSpy:
@@ -31,12 +40,16 @@ object ExecutionContextSpy:
   private case class BasicExecutionContextSpy(underlying: ExecutionContext) extends ExecutionContextSpy:
     private var _busyThreads: Set[Long] = Set()
     private var _maxConcurrency: Int = 0
+    private var _pendingTasks: Int = 0
 
     export underlying.reportFailure
     override def execute(runnable: Runnable): Unit =
+      synchronized { this._pendingTasks = this._pendingTasks + 1 }
       underlying.execute(() => { beforeExecute(); runnable.run(); afterExecute() })
-    override def busyThreads: Long = synchronized { this._busyThreads.size }
+    override def busyThreads: Int = synchronized { this._busyThreads.size }
     override def maxConcurrency: Int = synchronized { this._maxConcurrency }
+    override def pendingTasks: Int = synchronized { this._pendingTasks }
+    override def awaitTaskCompletion(): Unit = synchronized { while(this._pendingTasks > 0){ this.wait() } }
 
     /** Callback executed before the execution of each task. */
     private def beforeExecute(): Unit = synchronized {
@@ -47,4 +60,6 @@ object ExecutionContextSpy:
     /** Callback executed after the execution of each task. */
     private def afterExecute(): Unit = synchronized {
       this._busyThreads = this._busyThreads - Thread.currentThread().threadId()
+      this._pendingTasks = this._pendingTasks - 1
+      this.notifyAll()
     }
--- src/test/scala/it/unibo/distributedfrp/test/utils/executor/ExecutionContextSpyTest.scala
+++ src/test/scala/it/unibo/distributedfrp/test/utils/executor/ExecutionContextSpyTest.scala
@@ -10,28 +10,41 @@ class ExecutionContextSpyTest extends AbstractTest:
   private val nThreads: Int = Runtime.getRuntime.availableProcessors() + 1
   private val executor: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(nThreads))
 
-  "An ExecutionContextSpy" should "have no busy threads when created" in {
+  "An ExecutionContextSpy" should "have no pending tasks when created" in {
+    val spy: ExecutionContextSpy = ExecutionContextSpy(executor)
+    spy.pendingTasks shouldEqual 0
+  }
+
+  it should "keep track of the number of pending tasks" in {
+    val spy: ExecutionContextSpy = ExecutionContextSpy(executor)
+    val semaphore: Semaphore = Semaphore(0)
+    Seq.range(0, nThreads)
+      .map(taskId =>
+        spy.execute(() => semaphore.acquire())
+        spy.pendingTasks shouldEqual taskId + 1
+      )
+    semaphore.release(nThreads)
+    spy.awaitTaskCompletion()
+    spy.pendingTasks shouldEqual 0
+  }
+
+  it should "have no busy threads when created" in {
     val spy: ExecutionContextSpy = ExecutionContextSpy(executor)
     spy.busyThreads shouldEqual 0
   }
 
   it should "keep track of the number of threads that are currently busy executing some tasks" in {
     val spy: ExecutionContextSpy = ExecutionContextSpy(executor)
-    given ExecutionContext = spy
     val semaphore: Semaphore = Semaphore(0)
-    val tasksCompletion: Future[?] =
-      Future.sequence(
-        Seq.range(0, nThreads)
-          .map(taskId => {
-            val (ready, completion) = (Promise[Unit](), Promise[Unit]())
-            spy.execute(() => { ready.success(()); semaphore.acquire(); completion.success(()) })
-            Await.ready(ready.future, Defaults.timeout)
-            spy.busyThreads shouldEqual taskId + 1
-            completion.future
-          })
+    Seq.range(0, nThreads)
+      .map(taskId =>
+        val ready = Promise[Unit]()
+        spy.execute(() => { ready.success(()); semaphore.acquire() })
+        Await.ready(ready.future, Defaults.timeout)
+        spy.busyThreads shouldEqual taskId + 1
       )
     semaphore.release(nThreads)
-    Await.ready(tasksCompletion, Defaults.timeout)
+    spy.awaitTaskCompletion()
     spy.busyThreads shouldEqual 0
   }
 
@@ -42,20 +55,15 @@ class ExecutionContextSpyTest extends AbstractTest:
 
   it should "keep track of the maximum number of busy threads ever recorded" in {
     val spy: ExecutionContextSpy = ExecutionContextSpy(executor)
-    given ExecutionContext = spy
     val semaphore: Semaphore = Semaphore(0)
-    val tasksCompletion: Future[?] =
-      Future.sequence(
-        Seq.range(0, nThreads)
-          .map(taskId => {
-            val (ready, completion) = (Promise[Unit](), Promise[Unit]())
-            spy.execute(() => { ready.success(()); semaphore.acquire(); completion.success(()) })
-            Await.ready(ready.future, Defaults.timeout)
-            spy.maxConcurrency shouldEqual taskId + 1
-            completion.future
-          })
+    Seq.range(0, nThreads)
+      .map(taskId =>
+        val ready = Promise[Unit]()
+        spy.execute(() => { ready.success(()); semaphore.acquire() })
+        Await.ready(ready.future, Defaults.timeout)
+        spy.maxConcurrency shouldEqual taskId + 1
       )
     semaphore.release(nThreads)
-    Await.ready(tasksCompletion, Defaults.timeout)
+    spy.awaitTaskCompletion()
     spy.maxConcurrency shouldEqual nThreads
   }
